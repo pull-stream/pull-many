e2288ab041b4483c5d27cee61223acadbb990bf7
Dominic Tarr <dominic.tarr@gmail.com>
2014-06-05 19:16:37 +0000

    use latest interleavings

--- test/interleavings.js
+++ test/interleavings.js
@@ -4,37 +4,19 @@ var async = require('interleavings')
 var many = require('../')
 var assert = require('assert')
 
-async.test(strange, function (err, results, stats) {
-    console.log(results)
-    assert.equal(stats.failures, 0)
-  })
+async.test(function (async) {
 
-function strange (async) {
-
-    function p (read) {
-      return function (abort, cb) {
-        read(abort, async(cb))
-      }
-    }
 
     pull(
       //pull many must return a result in the same partial order.
       //so if we have a stream of even and a stream of odd numbers
       //then those should be in the same order in the output.
       many([
-        pull.values([1,3,5,7]),
-        pull.values([2,4,6,8])
-      ].map(p)),
-      function (read) {
-        return function (abort, cb) {
-          read(abort, function (end, data) {
-            console.log(end, data)
-            cb(end, data)
-          })
-        }
-      },
+        async.through('odd')(pull.values([1,3,5,7])),
+        async.through('even')(pull.values([2,4,6,8]))
+      ]),
+      async.through('collector'),
       pull.collect(function (err, ary) {
-        console.log(ary)
         var odd  = ary.filter(function (e) { return e % 2 })
         var even = ary.filter(function (e) { return !(e % 2) })
 
@@ -44,7 +26,7 @@ function strange (async) {
       })
     )
 
-  }
+  })
 
 
 //strange(async(54, console.error))
